3de53d566d010c26bc4b0f10adf7d70e765ced33
notcarbide <luis@0k.cx>
2022-05-18 10:53:32 -0500

    Create initial index
    Signed-off-by: notcarbide <luis@0k.cx>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
-import { Anchor, Box, Heading, Paragraph } from 'grommet'
+import Image from 'next/image'
+import { Box, Heading, Text } from 'grommet'
+import main from '../assets/main.jpg'
 
 export default function Home() {
   return (
-    <Box align="center" margin="large">
-      <Heading>Grommet is awesome!</Heading>
-      <Paragraph>
-        Find out more at{' '}
-        <Anchor href="https://v2.grommet.io/">https://v2.grommet.io/</Anchor>
-      </Paragraph>
+    <Box
+      align="center"
+      margin="small"
+    >
+      <Heading>C A R B I D E</Heading>
+      <Box
+        pad={{
+          bottom: "medium"
+        }}
+      >
+        <Image
+          alt="Main Carbide Image"
+          src={main}
+          width={360}
+          height={360}
+        />
+      </Box>
     </Box>
   )
 }
