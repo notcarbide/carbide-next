e0a7840071b1309ea00ef2e1251c8b43613095b5
notcarbide <luis@0k.cx>
2022-05-23 11:53:41 -0500

    YouTube: Adjust the styling of the YouTube player
    Use grommet Box to style.
    Make width responsive.
    Fix-up some errors about incorrect variables.
    
    Signed-off-by: notcarbide <luis@0k.cx>

--- components/common/YouTube.js
+++ components/common/YouTube.js
@@ -1,22 +1,24 @@
+import { Box } from 'grommet'
+
 const YouTube = ({ id }) => {
   const source = `https://www.youtube.com/embed/${id}`
 
-  if (!source)
-    return (<div>Loading...</div>)
-
   return (
-    <div>
+    <Box
+      width="large"
+      height="medium"
+    >
       <iframe
-        width="560"
-        height="315"
+        width="100%"
+        height="100%"
         src={source}
-        title="YouTube video player"
-        frameborder="0"
+        title="YouTube Video Player"
+        frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
+        allowFullScreen
       >
       </iframe>
-    </div>
+    </Box>
   )
 }
 
