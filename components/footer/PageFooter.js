6a1be6d69652b7c71709ab5aba4e22fcb3af3902
notcarbide <luis@0k.cx>
2022-05-24 13:14:58 -0500

    Footer: Add copyright for BadIn.tel Dev
    Working on this under the new project.
    
    Signed-off-by: notcarbide <luis@0k.cx>

--- components/footer/PageFooter.js
+++ components/footer/PageFooter.js
@@ -1,4 +1,4 @@
-import { Box, Button, Footer } from 'grommet'
+import { Box, Button, Footer, Text } from 'grommet'
 import { Facebook, Github, Instagram, Spotify,
          Youtube } from 'grommet-icons'
 
@@ -6,31 +6,41 @@ const PageFooter = () => {
   return (
     <Footer
       alignSelf="center"
+      direction="column"
+      gap="medium"
       pad={{
         top: "medium",
-        bottom: "small",
+        bottom: "medium",
       }}
     >
-      <Button
-        icon={<Spotify color="brand" />}
-        href="https://open.spotify.com/artist/46FMfS2QJiBsbD42BYviN0"
-      />
-      <Button
-        icon={<Facebook color="brand" />}
-        href="https://facebook.com/carbideband"
-      />
-      <Button
-        icon={<Instagram  color="brand" />}
-        href="https://instagram.com/carbide_band"
-      />
-      <Button
-        icon={<Youtube color="brand" />}
-        href="https://youtube.com/channel/UCqQJBHyLgElK4gSFF2d5d-g"
-      />
-      <Button
-        icon={<Github color="brand" />}
-        href="https://github.com/carbideband"
-      />
+      <Box
+        direction="row"
+        gap="medium"
+      >
+        <Button
+          icon={<Spotify color="brand" />}
+          href="https://open.spotify.com/artist/46FMfS2QJiBsbD42BYviN0"
+        />
+        <Button
+         icon={<Facebook color="brand" />}
+          href="https://facebook.com/carbideband"
+        />
+        <Button
+          icon={<Instagram  color="brand" />}
+          href="https://instagram.com/carbide_band"
+        />
+        <Button
+          icon={<Youtube color="brand" />}
+          href="https://youtube.com/channel/UCqQJBHyLgElK4gSFF2d5d-g"
+        />
+        <Button
+          icon={<Github color="brand" />}
+          href="https://github.com/carbideband"
+        />
+      </Box>
+      <Text size="small">
+        Copyright 2022 BadIn.tel Dev
+      </Text>
     </Footer>
   )
 }
